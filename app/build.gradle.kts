1dcaff00079b0a5319f6b2f3c0975a4d77f00275
slothdeboss <valentinvolodarskiy@gmail.com>
2023-09-10 18:05:28 +0300

    Added CirceIndicatorRow, RoundedCornerButton and RoundedCornerOutlinedButton as separate components. Implemented OnboardingScreen and StartScreen. Added required resources

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,16 +49,26 @@ android {
     }
 }
 
+val lifecycleVersion = "2.6.1"
+val coroutinesVersion = "1.7.1"
+
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
+
+    // Lifecycle
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
+
+    // Compose
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/slothdeboss/enefte/ui/util/values/PaddingModifiers.kt
@@ -0,0 +1,8 @@
+package com.slothdeboss.enefte.ui.util.values
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+val VerticalPadding16 = Modifier.padding(vertical = 16.dp)
+val VerticalPadding24 = Modifier.padding(vertical = 24.dp)
