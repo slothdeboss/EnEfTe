de1e8ed87d3c5961fa1aada3e3a8258c3af803ff
slothdeboss <valentinvolodarskiy@gmail.com>
2023-10-01 20:11:48 +0300

    Use IconButton instead of custom Box implementation in TitleTopAppBar

--- app/src/main/java/com/slothdeboss/enefte/ui/components/topAppBar/TitleTopAppBar.kt
+++ app/src/main/java/com/slothdeboss/enefte/ui/components/topAppBar/TitleTopAppBar.kt
@@ -2,21 +2,15 @@ package com.slothdeboss.enefte.ui.components.topAppBar
 
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import com.slothdeboss.enefte.R
-import com.slothdeboss.enefte.ui.screens.setupProfile.SetupProfileEvent
 import com.slothdeboss.enefte.ui.theme.EnEfTeTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -37,14 +31,7 @@ fun TitleTopAppBar(
             )
         },
         navigationIcon = {
-            Box(
-                modifier = Modifier
-                    .size(40.dp)
-                    .clickable {
-                        onBackPressed()
-                    },
-                contentAlignment = Alignment.Center
-            ) {
+            IconButton(onClick = onBackPressed) {
                 Image(
                     painter = painterResource(id = R.drawable.ic_back),
                     contentDescription = null
